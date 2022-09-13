f69b262ea62f5e600a24ba298b8916f03a181473
Mehedi Bhuiyan <mehedibuiyan734@gmail.com>
2022-09-14 01:15:09 +0600

    second commit

--- index.js
+++ index.js
@@ -2,29 +2,64 @@ const loginContainer  = document.querySelector(".login_container");
 const loginBtn = document.querySelector('.login_btn');
 const mainPage = document.querySelector(".main_page")
 
-// loginBtn.addEventListener("click", () => {
-//         let userName = document.querySelector(".username").value;
-//         let passWord = document.querySelector(".password").value;
-//     if(userName === "") {
-//         alert("Please Enter Username")
-//     }   
+loginBtn.addEventListener("click", () => {
+        let userName = document.querySelector(".username").value;
+        let passWord = document.querySelector(".password").value;
+    if(userName === "") {
+        alert("Please Enter Username")
+    }   
 
-//     else if (passWord === "") {
-//         alert("Please Enter your password")
-//     }
+    else if (passWord === "") {
+        alert("Please Enter your password")
+    }
 
-//     else{
-//         loginContainer.style.display = "none"
-//         mainPage.style.display = "block"
-//     }
-// })
-
-
-const depositBtn = document.querySelector(".deposit_btn");
-const withdrawValue = document.querySelector(".withdraw_value");
-
-depositBtn.addEventListener("click", () => {
-    let dipositValue = document.querySelector(".deposite_value").value;
-    console.log(dipositValue);
-
+    else{
+        loginContainer.style.display = "none"
+        mainPage.style.display = "block"
+    }
 })
+
+// Total_Amount
+    const total_amount= document.getElementById('total_amount').innerHTML;
+    const total_newAmount = parseFloat(total_amount)
+// Deposite
+    const depositBtn = document.querySelector(".deposit_btn");
+
+    depositBtn.addEventListener("click", () => {
+        let depositValue = document.querySelector(".deposite_value").value;
+        let depositeAmount = document.getElementById("deposite_amount").innerHTML;
+
+        let depositNewValue = parseFloat(depositValue);
+        let depositeNewAmount = parseFloat(depositeAmount);
+
+        let total_deposite =  depositNewValue + depositeNewAmount;
+        console.log(total_deposite);
+        document.getElementById("deposite_amount").innerHTML = total_deposite;
+
+        document.getElementById('total_amount').innerHTML = total_newAmount + total_deposite ;
+
+        document.querySelector(".deposite_value").value= "";
+
+    })
+
+// Withdraw
+
+    const withdrawBtn = document.querySelector(".withdraw_btn");
+
+    withdrawBtn.addEventListener("click", () => {
+        let withdraw_amount = document.getElementById("withdraw_amount").innerHTML;
+        let withdraw_value = document.querySelector('.withdraw_value').value;
+
+        let withdraw_newAmount = parseFloat(withdraw_amount);
+        let withdraw_newValue = parseFloat(withdraw_value);
+        
+        let total_withdraw = withdraw_newAmount + withdraw_newValue;
+        document.getElementById("withdraw_amount").innerHTML = total_withdraw;
+
+        document.getElementById('total_amount').innerHTML = total_newAmount - total_withdraw;
+
+        document.querySelector('.withdraw_value').value = "";
+
+    })
+
+    
